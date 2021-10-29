91675fe3becfb7b423034b21503bcae9534e71db
katrenadel <93194865+katrenadel@users.noreply.github.com>
2021-10-29 23:59:18 +0200

    Add files via upload

---
+++ task1dsp.m
@@ -0,0 +1,31 @@
+ A=[-7 5 -9 ;2 -1 2 ;1 -1 2];
+disp(A);
+
+
+B=[16 3 2 13 ;5 10 11 8 ;9 6 7 12 ;4 15 14 1];
+disp(B);
+
+
+C=[4 2 -3;7 -7 9;3 -5 6];
+disp(C);
+
+D=[6 3 2;2 12 -7;-1 6 2;-5 15 11];
+disp(D);
+3*A-5*C
+7*A+2*B %matrix dimention must agree
+C*A
+C*D'
+zeros(3)
+zeros(2,3)
+ones(3)
+ones(2,3)
+size(D)
+zeros(size(D))
+diag([1 2 3 4])
+eye(3)
+[A,B] %ERORR
+[A;B]%ERORR
+x=diage([5 5 5 5 5 5 5)]
+x(1:7,8)=[5;5;5;5;5;5;5]
+A(1,:)
+A(:,1)
